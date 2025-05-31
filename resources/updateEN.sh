98424c6ac7a054ccd81f0906145e09647140884b
Kenny <KennyAwesome@users.noreply.github.com>
2025-06-01 01:18:40 +0200

    Update updateEN.sh

--- resources/updateEN.sh
+++ resources/updateEN.sh
@@ -1,6 +1,7 @@
 rm en.txt
 bash composeBook.sh > en.txt
-pandoc en.txt -o ./book/42_en_latest.pdf
+#pandoc en.txt -o ./book/42_en_latest.pdf
+pandoc -V geometry:margin=1in -o ./book/42_en_latest.pdf en.txt
 git add ./book/42_en_latest.pdf
 git commit -m "Updated edition"
 git push
